9ba32e6734ea1d0f54c3454cf205938300a047d3
imran hossain <imranme.global@gmail.com>
2026-05-10 19:04:00 +0600

    file init

---
+++ file1.js
@@ -0,0 +1,18 @@
+var pr1 = 10;
+var pr1 = 20;
+
+console.log(pr1);
+
+// IIFE 
+
+(() => {
+    let pr1 = 40;
+    console.log(pr1)
+
+})();
+
+(() => {
+    let pr1 = 60;
+    console.log(pr1)
+
+})();
