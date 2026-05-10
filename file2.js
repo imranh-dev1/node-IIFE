3841f67c976eae36fe9186daa4361a45ff50aa37
imran hossain <imranme.global@gmail.com>
2026-05-10 19:07:40 +0600

    IIFE Explore

---
+++ file2.js
@@ -0,0 +1,21 @@
+function name() {
+    // console.log("Rahim");
+}
+
+
+function name() {
+    // console.log("Karim");
+}
+
+// name()
+
+// IIFE 
+
+(function name() {
+    console.log("Rahim");
+})();
+
+
+(function name() {
+    console.log("Karim");
+})();
